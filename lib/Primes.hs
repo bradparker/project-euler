633eb35879f84a164ab58f4554f7ce164b7959f1
Brad Parker <hi@bradparker.com>
2021-06-25 20:30:10 +1000

    More infix, tidier primes

--- lib/Primes.hs
+++ lib/Primes.hs
@@ -3,24 +3,24 @@
 module Primes where
 
 import Numeric.Natural (Natural)
-import Stream (Stream (Stream))
+import Stream (Stream ((:>)), (\\))
 import qualified Stream
 
 primeFactors :: Natural -> [Natural]
 primeFactors = go primes
   where
     go _ 1 = []
-    go (Stream p ps) n =
+    go (p :> ps) n =
       let (n', r) = divMod n p
        in if r == 0
-            then p : go (Stream p ps) n'
+            then p : go (p :> ps) n'
             else go ps n
 
-composites :: Stream Natural
-composites = Stream.mergeAll (mulitples <$> primes)
-  where
-    mulitples :: Natural -> Stream Natural
-    mulitples n = Stream.iterate (+ n) (n * n)
-
 primes :: Stream Natural
-primes = Stream 2 (Stream.iterate (+ 1) 3 `Stream.difference` composites)
+primes = 2 :> (Stream.iterate (+ 1) 3 \\ composites)
+  where
+    composites :: Stream Natural
+    composites = Stream.unionAll (multiples <$> primes)
+
+    multiples :: Natural -> Stream Natural
+    multiples n = Stream.iterate (+ n) (n * n)
--- lib/Stream.hs
+++ lib/Stream.hs
@@ -6,40 +6,49 @@ module Stream where
 import Numeric.Natural (Natural)
 import Prelude hiding (iterate, take, takeWhile, (!!))
 
-data Stream a = Stream a (Stream a)
+data Stream a = a :> Stream a
 
 iterate :: (a -> a) -> a -> Stream a
-iterate f a = Stream a (iterate f (f a))
+iterate f a = a :> iterate f (f a)
 
 take :: Natural -> Stream a -> [a]
 take 0 _ = []
-take n (Stream x xs) = x : take (n - 1) xs
+take n (x :> xs) = x : take (n - 1) xs
 
 (!!) :: Stream a -> Natural -> a
-Stream x xs !! 0 = x
-Stream x xs !! n = xs !! (n - 1)
+(x :> _) !! 0 = x
+(_ :> xs) !! n = xs !! (n - 1)
 
 takeWhile :: (a -> Bool) -> Stream a -> [a]
-takeWhile p (Stream x xs)
+takeWhile p (x :> xs)
   | p x = x : takeWhile p xs
   | otherwise = []
 
 instance Functor Stream where
   fmap :: (a -> b) -> Stream a -> Stream b
-  fmap f (Stream x xs) = Stream (f x) (fmap f xs)
+  fmap f (x :> xs) = f x :> fmap f xs
 
-difference :: Ord a => Stream a -> Stream a -> Stream a
-difference (Stream x xs) (Stream y ys)
-  | x < y = Stream x (difference xs (Stream y ys))
-  | x == y = difference xs ys
-  | x > y = difference (Stream x xs) ys
+(\\) :: Ord a => Stream a -> Stream a -> Stream a
+(\\) (x :> xs) (y :> ys) =
+  case compare x y of
+    LT -> x :> (xs \\ (y :> ys))
+    EQ -> xs \\ ys
+    GT -> (x :> xs) \\ ys
 
-merge :: Ord a => Stream a -> Stream a -> Stream a
-merge (Stream x xs) (Stream y ys)
-  | x < y = Stream x (merge xs (Stream y ys))
-  | x == y = Stream x (merge xs ys)
-  | x > y = Stream y (merge (Stream x xs) ys)
+union :: Ord a => Stream a -> Stream a -> Stream a
+union (x :> xs) (y :> ys) =
+  case compare x y of
+    LT -> x :> (xs `union` (y :> ys))
+    EQ -> x :> (xs `union` ys)
+    GT -> y :> ((x :> xs) `union` ys)
 
-mergeAll :: Ord a => Stream (Stream a) -> Stream a
-mergeAll (Stream (Stream x xs) xss) =
-  Stream x (merge xs (mergeAll xss))
+unionAll :: Ord a => Stream (Stream a) -> Stream a
+unionAll ((x :> xs) :> xss) = x :> (xs `union` unionAll xss)
+
+-- So this is unproductive and I'd like to properly understand why
+--
+-- fold :: (a -> b -> b) -> Stream a -> b
+-- fold f (x :> xs) = x `f` fold f xs
+
+-- unionAll' :: Ord a => Stream (Stream a) -> Stream a
+-- unionAll' = fold (\(x :> xs) xss -> x :> xs `union` xss)
