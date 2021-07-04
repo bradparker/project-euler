ffd01248c019ec8833e3271e3e23d6ed74897c20
Brad Parker <hi@bradparker.com>
2021-07-04 21:22:32 +1000

    Inverted collatz ... for curiosity's sake

--- 014/Main.hs
+++ 014/Main.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE TupleSections #-}
 {-# OPTIONS_GHC -Wall #-}
 
 module Main where
@@ -24,14 +25,15 @@ module Main where
 
 import Control.Monad.State (State, execState, gets, modify)
 import Data.Function (on)
+import Data.Functor.Base (ListF (Cons, Nil), TreeF (NodeF))
+import Data.Functor.Foldable (unfold)
 import Data.IntMap (IntMap)
 import qualified Data.IntMap as IntMap
 import Data.List (maximumBy)
-
-collatz :: Int -> Int
-collatz n
-  | even n = n `div` 2
-  | otherwise = 3 * n + 1
+import Data.Set (Set)
+import qualified Data.Set as Set
+import Data.Tree (Tree)
+import Numeric.Natural (Natural)
 
 collatzSizes :: [Int] -> IntMap Int
 collatzSizes ns = execState (traverse collatzSize ns) IntMap.empty
@@ -42,7 +44,7 @@ collatzSizes ns = execState (traverse collatzSize ns) IntMap.empty
       existing <- gets (IntMap.lookup n)
       case existing of
         Nothing -> do
-          size <- (1 +) <$> collatzSize (collatz n)
+          size <- (1 +) <$> collatzSize (fromIntegral (collatz (fromIntegral n)))
           modify (IntMap.insert n size)
           pure size
         Just size -> pure size
@@ -58,5 +60,77 @@ solve n =
     . enumFromTo 2
     $ (n - 1)
 
+collatz :: Natural -> Natural
+collatz n
+  | even n = n `div` 2
+  | otherwise = 3 * n + 1
+
+collatzSequence :: Natural -> [Natural]
+collatzSequence n0 = n0 : unfold alg n0
+  where
+    alg :: Natural -> ListF Natural Natural
+    alg 1 = Nil
+    alg n = let c = collatz n in Cons c c
+
+invertedCollatz :: Natural -> Tree Natural
+invertedCollatz bound = unfold alg (1, Set.singleton 1)
+  where
+    alg :: (Natural, Set Natural) -> TreeF Natural (Natural, Set Natural)
+    alg (n, seen) =
+      NodeF
+        n
+        ( if n > bound
+            then []
+            else
+              map
+                (,Set.fromList next <> seen)
+                (filter (not . (`Set.member` seen)) next)
+        )
+      where
+        next :: [Natural]
+        next = n * 2 : [(n - 1) `div` 3 | n > 1 && (n - 1) `mod` 3 == 0]
+
+-- >>> collatzSequence 13
+-- [13,40,20,10,5,16,8,4,2,1]
+-- >>> putStrLn (Tree.drawTree (fmap show (invertedCollatz 40)))
+--  1
+--  |
+--  `- 2
+--     |
+--     `- 4
+--        |
+--        `- 8
+--           |
+--           `- 16
+--              |
+--              +- 32
+--              |  |
+--              |  `- 64
+--              |
+--              `- 5
+--                 |
+--                 `- 10
+--                    |
+--                    +- 20
+--                    |  |
+--                    |  `- 40
+--                    |     |
+--                    |     +- 80
+--                    |     |
+--                    |     `- 13
+--                    |        |
+--                    |        `- 26
+--                    |           |
+--                    |           `- 52
+--                    |
+--                    `- 3
+--                       |
+--                       `- 6
+--                          |
+--                          `- 12
+--                             |
+--                             `- 24
+--                                |
+--                                `- 48
 main :: IO ()
 main = print (solve 1000000)
--- shell.nix
+++ shell.nix
@@ -1,3 +1,15 @@
-{ nixpkgs ? import <nixpkgs> { } }:
+{ nixpkgs ? import <nixpkgs> {
+  overlays = [
+    (self: super: {
+      haskellPackages = super.haskellPackages.override {
+        overrides = hself: hsuper: {
+          recursion-schemes = hsuper.callCabal2nix "recursion-schemes" (
+            builtins.fetchTarball "https://github.com/recursion-schemes/recursion-schemes/archive/30767f798889364d87ff874c0dd46972c4ab2f40.tar.gz"
+          ) {};
+        };
+      };
+    })
+  ];
+} }:
 let package = nixpkgs.callPackage ./. { };
 in package.env
