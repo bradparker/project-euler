3412a0cde815bde12dc61a43c27299a102bef8ad
Brad Parker <hi@bradparker.com>
2021-07-05 20:29:07 +1000

    More collatz fun

--- 014/Main.hs
+++ 014/Main.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TupleSections #-}
 {-# OPTIONS_GHC -Wall #-}
 
@@ -23,28 +24,30 @@ module Main where
 -- NOTE: Once the chain starts the terms are allowed to go above one
 -- million.
 
+import Control.Monad.Reader (Reader, ask, local, runReader)
 import Control.Monad.State (State, execState, gets, modify)
 import Data.Function (on)
 import Data.Functor.Base (ListF (Cons, Nil), TreeF (NodeF))
-import Data.Functor.Foldable (unfold)
+import Data.Functor.Foldable (fold, unfold)
 import Data.IntMap (IntMap)
 import qualified Data.IntMap as IntMap
-import Data.List (maximumBy)
+import Data.List (genericLength, maximumBy)
 import Data.Set (Set)
 import qualified Data.Set as Set
-import Data.Tree (Tree)
+import Data.Tree (Tree (Node))
+import qualified Data.Tree as Tree
 import Numeric.Natural (Natural)
 
 collatzSizes :: [Int] -> IntMap Int
-collatzSizes ns = execState (traverse collatzSize ns) IntMap.empty
+collatzSizes ns = execState (traverse collatzSize' ns) IntMap.empty
   where
-    collatzSize :: Int -> State (IntMap Int) Int
-    collatzSize 1 = pure 1
-    collatzSize n = do
+    collatzSize' :: Int -> State (IntMap Int) Int
+    collatzSize' 1 = pure 1
+    collatzSize' n = do
       existing <- gets (IntMap.lookup n)
       case existing of
         Nothing -> do
-          size <- (1 +) <$> collatzSize (fromIntegral (collatz (fromIntegral n)))
+          size <- (1 +) <$> collatzSize' (collatz n)
           modify (IntMap.insert n size)
           pure size
         Just size -> pure size
@@ -60,77 +63,105 @@ solve n =
     . enumFromTo 2
     $ (n - 1)
 
-collatz :: Natural -> Natural
+collatz :: Integral n => n -> n
 collatz n
   | even n = n `div` 2
   | otherwise = 3 * n + 1
 
-collatzSequence :: Natural -> [Natural]
+collatzSequence :: forall n. Integral n => n -> [n]
 collatzSequence n0 = n0 : unfold alg n0
   where
-    alg :: Natural -> ListF Natural Natural
+    alg :: n -> ListF n n
     alg 1 = Nil
     alg n = let c = collatz n in Cons c c
 
-invertedCollatz :: Natural -> Tree Natural
-invertedCollatz bound = unfold alg (1, Set.singleton 1)
+acyclicInvertedCollatz :: Tree Natural
+acyclicInvertedCollatz = unfold alg (1, Set.singleton 1)
   where
     alg :: (Natural, Set Natural) -> TreeF Natural (Natural, Set Natural)
     alg (n, seen) =
       NodeF
         n
-        ( if n > bound
-            then []
-            else
-              map
-                (,Set.fromList next <> seen)
-                (filter (not . (`Set.member` seen)) next)
+        ( map
+            (,Set.fromList next <> seen)
+            (filter (not . (`Set.member` seen)) next)
         )
       where
         next :: [Natural]
         next = n * 2 : [(n - 1) `div` 3 | n > 1 && (n - 1) `mod` 3 == 0]
 
+invertedCollatz :: Tree Natural
+invertedCollatz = unfold alg 1
+  where
+    alg :: Natural -> TreeF Natural Natural
+    alg n = NodeF n next
+      where
+        next :: [Natural]
+        next = n * 2 : [(n - 1) `div` 3 | n > 1 && (n - 1) `mod` 3 == 0]
+
+toLevel :: forall a. Natural -> Tree a -> Tree a
+toLevel target = flip runReader 0 . fold alg
+  where
+    alg :: TreeF a (Reader Natural (Tree a)) -> Reader Natural (Tree a)
+    alg (NodeF a as) = do
+      level <- ask
+      if level >= target
+        then pure (Node a [])
+        else Node a <$> traverse (local (+ 1)) as
+
+invertedCollatzLevels :: [Set Natural]
+invertedCollatzLevels = Set.fromList <$> Tree.levels invertedCollatz
+
+collatzSize :: Natural -> Natural
+collatzSize n = genericLength (takeWhile (Set.notMember n) invertedCollatzLevels)
+
 -- >>> collatzSequence 13
 -- [13,40,20,10,5,16,8,4,2,1]
--- >>> putStrLn (Tree.drawTree (fmap show (invertedCollatz 40)))
---  1
---  |
---  `- 2
---     |
---     `- 4
---        |
---        `- 8
---           |
---           `- 16
---              |
---              +- 32
---              |  |
---              |  `- 64
---              |
---              `- 5
---                 |
---                 `- 10
---                    |
---                    +- 20
---                    |  |
---                    |  `- 40
---                    |     |
---                    |     +- 80
---                    |     |
---                    |     `- 13
---                    |        |
---                    |        `- 26
---                    |           |
---                    |           `- 52
---                    |
---                    `- 3
---                       |
---                       `- 6
---                          |
---                          `- 12
---                             |
---                             `- 24
---                                |
---                                `- 48
+-- >>> putStrLn (Tree.drawTree (fmap show (toLevel 9 acyclicInvertedCollatz)))
+--   1
+--   |
+--   `- 2
+--      |
+--      `- 4
+--         |
+--         `- 8
+--            |
+--            `- 16
+--               |
+--               +- 32
+--               |  |
+--               |  `- 64
+--               |     |
+--               |     +- 128
+--               |     |  |
+--               |     |  `- 256
+--               |     |     |
+--               |     |     +- 512
+--               |     |     |
+--               |     |     `- 85
+--               |     |
+--               |     `- 21
+--               |        |
+--               |        `- 42
+--               |           |
+--               |           `- 84
+--               |
+--               `- 5
+--                  |
+--                  `- 10
+--                     |
+--                     +- 20
+--                     |  |
+--                     |  `- 40
+--                     |     |
+--                     |     +- 80
+--                     |     |
+--                     |     `- 13
+--                     |
+--                     `- 3
+--                        |
+--                        `- 6
+--                           |
+--                           `- 12
 main :: IO ()
 main = print (solve 1000000)
