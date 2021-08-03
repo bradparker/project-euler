a660fd96d343c623381407b10f517eb82979f752
Brad Parker <hi@bradparker.com>
2021-08-03 19:24:39 +1000

    Make it easier to change compiler versions

--- shell.nix
+++ shell.nix
@@ -1,11 +1,14 @@
-{ nixpkgs ? import <nixpkgs> {
+let
+  recursion-schemes-source = builtins.fetchTarball
+    "https://github.com/recursion-schemes/recursion-schemes/archive/30767f798889364d87ff874c0dd46972c4ab2f40.tar.gz";
+in { nixpkgs ? import <nixpkgs> {
   overlays = [
     (self: super: {
-      haskellPackages = super.haskellPackages.override {
-        overrides = hself: hsuper: {
-          recursion-schemes = hsuper.callCabal2nix "recursion-schemes" (
-            builtins.fetchTarball "https://github.com/recursion-schemes/recursion-schemes/archive/30767f798889364d87ff874c0dd46972c4ab2f40.tar.gz"
-          ) {};
+      haskell = super.haskell // {
+        packageOverrides = hself: hsuper: {
+          recursion-schemes =
+            hsuper.callCabal2nix "recursion-schemes" recursion-schemes-source
+            { };
         };
       };
     })
